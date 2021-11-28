1e91e05717ae97b352a32b1e2c8db69fee1192e5
Kin Yuunan <yuunan.kin@gmail.com>
2021-11-28 09:57:43 +0900

    eat mushroom(custom action)&collide&headbutt

--- game.js
+++ game.js
@@ -30,7 +30,9 @@ loadSprite("blue-surprise", "RMqCc1G.png");
 let score = 0;
 let level = 1;
 const SPEED = 120;
-const JUMP = 360;
+const JUMP_FORCE = 400;
+const JUMP_FORCE_MAX = 550;
+let currentJumpForce = JUMP_FORCE;
 
 scene("game", () => {
   layers(["bg", "obj", "ui"], "obj");
@@ -50,7 +52,7 @@ scene("game", () => {
     width: 20,
     height: 20,
     "=": () => [sprite("block"), area(), solid()],
-    $: () => [sprite("coin"), "coin"],
+    $: () => [sprite("coin"), area(), "coin"],
     "%": () => [sprite("surprise"), area(), solid(), "coin-surprise"],
     "*": () => [sprite("surprise"), area(), solid(), "mushroom-surprise"],
     "}": () => [sprite("unboxed"), area(), solid()],
@@ -78,6 +80,30 @@ scene("game", () => {
     ],
     x: () => [sprite("blue-steel"), area(), solid(), scale(0.5)],
   };
+  function big() {
+    let timer = 0;
+    let isBig = false;
+    return {
+      update() {
+        timer -= dt();
+        if (timer <= 0) {
+          this.smallify();
+        }
+      },
+      smallify() {
+        isBig = false;
+        this.scale = vec2(1);
+        timer = 0;
+        currentJumpForce = JUMP_FORCE;
+      },
+      biggerify(time) {
+        isBig = true;
+        this.scale = vec2(2);
+        timer = time;
+        currentJumpForce = JUMP_FORCE_MAX;
+      },
+    };
+  }
   const gameLevel = addLevel(map, levelConfig);
   const player = add([
     sprite("mario"),
@@ -87,6 +113,7 @@ scene("game", () => {
     body(),
     pos(15, 30),
     origin("bot"),
+    big(),
   ]);
   const scoreLabel = add([
     text(score),
@@ -107,9 +134,56 @@ scene("game", () => {
     },
   ]);
 
+  player.onHeadbutt((obj) => {
+    if (obj.is("coin-surprise")) {
+      add([sprite("coin"), pos(obj.pos.sub(0, 20)), area(), "coin"]);
+      destroy(obj);
+      add([sprite("unboxed"), pos(obj.pos.sub(0, 0)), area(), solid()]);
+      return;
+    }
+    if (obj.is("mushroom-surprise")) {
+      destroy(obj);
+      add([sprite("unboxed"), pos(obj.pos.sub(0, 0)), area(), solid()]);
+      const mushroom = add([
+        sprite("mushroom"),
+        pos(obj.pos.sub(0, 20)),
+        area(),
+        solid(),
+        body(),
+        "mushroom",
+      ]);
+      mushroom.onUpdate((obj) => {
+        obj.move(SPEED * 0.5, 0);
+      });
+      return;
+    }
+  });
+
+  player.onCollide("coin", (obj) => {
+    destroy(obj);
+    score += 1;
+    scoreLabel.value = score;
+    scoreLabel.text = score;
+    return;
+  });
+  player.onCollide("mushroom", (obj) => {
+    destroy(obj);
+    player.biggerify(7);
+    return;
+  });
+  player.onCollide("dangerous", (obj) => {
+    destroy(obj);
+    player.solid = false;
+
+    setTimeout(() => {
+      reload();
+    }, 1000);
+    return;
+  });
+
   onKeyDown("space", () => {
     if (player.grounded()) {
-      player.jump(JUMP);
+      player.jump(currentJumpForce);
     }
   });
   onKeyDown("left", () => {
